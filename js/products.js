e763171e75159d430b6decde3424f9f93c7edeff
YI-JYUN HE <jobs.steve54@gmail.com>
2024-01-24 14:20:09 +0800

    [ feat ] 加入第三週作業

--- js/products.js
+++ js/products.js
@@ -1,5 +1,7 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 
+let productModal = null;
+let delProductModal = null;
 
 // 產品資料格式
 
@@ -9,7 +11,10 @@ createApp({
             apiUrl: 'https://vue3-course-api.hexschool.io/v2',
             apiPath: 'hexvue',
             products: [],
-            tempProduct: {},
+            isNew: false,
+            tempProduct: {
+                imagesUrl: [],
+            },
         }
     },
     methods: {
@@ -19,28 +24,84 @@ createApp({
                 .then(() => {
                     this.getdata();
                 })
-                .catch(() => {
+                .catch((err) => {
                     alert(err.respone.data.message);
                     window.location = "login.html";
                 })
         },
 
         getdata() {
-            const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
+            const url = `${this.apiUrl}/api/${this.apiPath}/admin/products/all`;
             axios.get(url)
-                .then((response) => {
-                    this.products = response.data.products;
+                .then((res) => {
+                    this.products = res.data.products;
+                    console.log(this.products)
                 })
                 .catch((err) => {
-                    alert(err.respone.data.message);
+                    alert(err.res.data.message);
                 })
         },
 
-        openProduct(item) {
-            this.tempProduct = item;
-        }
+        updateProduct() {
+            let url = `${this.apiUrl}/api/${this.apiPath}/admin/product`;
+            let http = "post";
+
+            if (!this.isNew) {
+                url = `${this.apiUrl}/api/${this.apiPath}/admin/product/${this.tempProduct.id}`;
+                http = 'put';
+            }
+
+            axios[http](url, {data: this.tempProduct}).then((res) => {
+                alert(res.data.message);
+                productModal.hide();
+                this.getdata();
+            }).catch((err) => {
+                alert(err.res.data.message);
+            }) 
+        },
+
+        openModal(isNew, item) {
+            if (isNew === "new") {
+                this.tempProduct = {
+                    imagesUrl: [],
+                };
+                this.isNew = true;
+                productModal.show();
+            } else if (isNew === "edit") {
+                this.tempProduct = {...item};
+                this.isNew = false;
+                productModal.show(); 
+            } else if (isNew === "delete") {
+                this.tempProduct = {...item};
+                delProductModal.show();
+            }
+        },
+
+        delProduct() {
+            const url = `${this.apiUrl}/api/${this.apiPath}/admin/product/${this.tempProduct.id}`;
+
+            axios.delete(url).then((res) => {
+                alert(res.data.message);
+                delProductModal.hide();
+                this.getdata();
+            }).catch((err) => {
+                alert(err.res.data.message)
+            })
+        },
+
+        createImage() {
+            this.tempProduct.imagesUrl = [];
+            this.tempProduct.imagesUrl.push("");
+        },
     },
     mounted() {
+        productModal = new bootstrap.Modal(document.getElementById("productModal"), {
+            keyboard: false
+        });
+
+        delProductModal = new bootstrap.Modal(document.getElementById("delProductModal"), {
+            keyboard: false
+        });
         // 取出 Token
         const token = document.cookie.replace(/(?:(?:^|.*;\s*)vueToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
         axios.defaults.headers.common.Authorization = token;
